ffc140a069e5b33060331bee13fa7d09c4beecfb
Dmitry <nedimo69rr@gmail.com>
2026-04-06 23:04:03 +0300

    Реализация регистарции и логина пользователя

--- database/databasemanager.cpp
+++ database/databasemanager.cpp
@@ -1,21 +1,44 @@
 #include "databasemanager.h"
 #include <QSqlQuery>
 #include <QSqlError>
+#include <QSqlRecord>
 #include <QUuid>
+#include <QDebug>
+#include <QDateTime>
+#include <QDir>
 
-DatabaseManager::DatabaseManager(QObject *parent):QObject(parent)
+DatabaseManager::DatabaseManager(QObject *parent)
+    :QObject(parent)
 {
     m_connectionName = QUuid::createUuid().toString(QUuid::WithoutBraces);
 }
 
-bool DatabaseManager::open(const QString &dbPath)
-{
-    m_db = QSqlDatabase.addDatabase("QSQLITE", m_connectionName);
-    m_db.setDatabaseName(dbPath);
+// ─── Tables ──────────────────────────────────────────────────────────────────
 
-    if (!m_db.isOpen())
+bool DatabaseManager::open(const QString &dbName)
+{
+    QString path = QDir::currentPath();
+    QDir dir(path);
+    if (!dir.exists())
+        dir.mkpath(".");
+
+    QString dbFile = path + "/" + dbName;
+    qDebug() << "[DB] path" << dbFile;
+
+    if (!QSqlDatabase::contains("CloudDiskDB"))
     {
-        qCritical() << "DB" << dbPath << "is not open!: " << m_db.lastError().text();
+        m_db = QSqlDatabase::addDatabase("QSQLITE", m_connectionName);
+    }
+    else
+    {
+        m_db = QSqlDatabase::database("CloudDiskDB");
+    }
+    m_db = QSqlDatabase::addDatabase("QSQLITE");
+    m_db.setDatabaseName(dbName);
+
+    if (!m_db.open())
+    {
+        qCritical() << "[DB]" << dbFile << "is not open!: " << m_db.lastError().text();
         return false;
     }
 
@@ -29,16 +52,16 @@ bool DatabaseManager::createTables()
     //Таблица пользователей
     if (!query.exec(R"(
         CREATE TABLE IF NOT EXISTS users
-        (id INTEGER    PRIMARY KEY    AUTOINCREMENT,
-        username    TEXT    NOT NULL    UNIQUE,
-        email   TEXT    NOT NULL    UNIQUE,
-        password_hash   TEXT NOT NULL,
-        salt TEXT   NOT NULL,
-        quota_bytes NOT NULL    DEFAULT 10737418240,
-        used_bytes  NOT NULL    DEFAULT 0,
-        is_active   NOT NULL    DEFAULT 1,
-        created_at  NOT NULL,
-        updated_at  NOT NULL
+        (id             INTEGER    PRIMARY KEY    AUTOINCREMENT,
+        username        TEXT    NOT NULL    UNIQUE,
+        email           TEXT    NOT NULL    UNIQUE,
+        password_hash   TEXT    NOT NULL,
+        salt            TEXT    NOT NULL,
+        quota_bytes     INTEGER NOT NULL    DEFAULT 10737418240,
+        used_bytes      INTEGER NOT NULL    DEFAULT 0,
+        is_active       BOOL    NOT NULL    DEFAULT 1,
+        created_at      TEXT    NOT NULL,
+        updated_at      TEXT    NOT NULL
         )
     )"))
     {
@@ -86,7 +109,7 @@ bool DatabaseManager::createTables()
         return false;
     }
 
-    qDebug() << "All tables was loaded";
+    qDebug() << "[DB] All tables was loaded";
     return true;
 }
 
@@ -96,3 +119,119 @@ void DatabaseManager::close()
         m_db.close();
     QSqlDatabase::removeDatabase(m_connectionName);
 }
+
+// ─── Sessions ─────────────────────────────────────────────────────────────────
+
+bool DatabaseManager::createSession(qint64 &userId, const QString &jwtToken, qint64 expiresAt)
+{
+    QSqlQuery q(m_db);
+    q.prepare(R"(INSERT INTO sessions (user_id, token, expires_at, created_at)
+                VALUES (:uid, :t, :exp, :ca)
+                )");
+
+    q.bindValue(":uid", userId);
+    q.bindValue(":t", jwtToken);
+    q.bindValue(":exp", expiresAt);
+    q.bindValue(":ca", QDateTime::currentDateTimeUtc().toString(Qt::ISODate));
+    return q.exec();
+}
+
+bool DatabaseManager::deleteSession(const QString &jwtToken)
+{
+    QSqlQuery q(m_db);
+    q.prepare("DELETE FROM sessions WHERE token = :t");
+    q.bindValue(":t", jwtToken);
+
+    return q.exec();
+}
+
+bool DatabaseManager::isSessionValid(const QString &jwtToken)
+{
+    QSqlQuery q(m_db);
+    q.prepare("SELECT expires_at FROM sessions WHERE token = :t");
+    q.bindValue(":t", jwtToken);
+
+    if (!q.exec() || !q.next())
+        return false;
+    qint64 exp = q.value(0).toLongLong();
+
+    return QDateTime::currentSecsSinceEpoch() < exp;
+}
+
+// ─── Users ───────────────────────────────────────────────────────────────────
+
+static User userFromQuery(const QSqlQuery &q)
+{
+    User u;
+    u.id = q.value("id").toLongLong();
+    u.username = q.value("username").toString();
+    u.email = q.value("email").toString();
+    u.passwordHash = q.value("password_hash").toString();
+    u.salt = q.value("salt").toString();
+    u.quotaBytes = q.value("quota_bytes").toLongLong();
+    u.usedBytes = q.value("used_bytes").toLongLong();
+    u.isActive = q.value("is_active").toBool();
+    u.createdAt = QDateTime::fromString(q.value("created_at").toString(), Qt::ISODate);
+    u.updatedAt = QDateTime::fromString(q.value("updated_at").toString(), Qt::ISODate);
+
+    return u;
+}
+
+optional<User> DatabaseManager::getUserByUsername(const QString &username)
+{
+    QSqlQuery query(m_db);
+    query.prepare(R"(SELECT *FROM users WHERE username = :u)");
+    query.bindValue(":u", username);
+
+    if (query.exec() && query.next())
+        return userFromQuery(query);
+    return nullopt;
+}
+
+optional<User> DatabaseManager::getUserById(const qint64 &id)
+{
+    QSqlQuery q(m_db);
+    q.prepare(R"(SELECT * FROM users WHERE id = :id)");
+    q.bindValue(":id", id);
+    if (q.exec() && q.next())
+        return userFromQuery(q);
+    return nullopt;
+}
+
+qint64 DatabaseManager::calcUsedBytes(const qint64 &id)
+{
+    QSqlQuery q(m_db);
+    q.prepare(R"(SELECT used_bytes FROM users WHERE id = :id)");
+    q.bindValue(":id", id);
+
+    if (!q.exec() || !q.next())
+        return 0;
+    return q.value(0).toLongLong();
+}
+
+optional<User> DatabaseManager::createUser(const QString &username, const QString &email,
+                                                const QString &hash, const QString &salt,
+                                                qint64 defaultQuota)
+{
+    QSqlQuery query(m_db);
+    query.prepare(R"(INSERT INTO users (username, email, password_hash, salt, quota_bytes, created_at, updated_at)
+                  VALUES (:u, :e, :ph, :s, :qb, :ca, :ua)
+                  )");
+    QString now = QDateTime::currentDateTimeUtc().toString(Qt::ISODate);
+    query.bindValue(":u", username);
+    query.bindValue(":e", email);
+    query.bindValue(":ph", hash);
+    query.bindValue(":s", salt);
+    query.bindValue(":qb", defaultQuota);
+    query.bindValue(":ca", now);
+    query.bindValue(":ua", now);
+
+    if (!query.exec())
+    {
+        qDebug() << "[DB] createUser:" << query.lastError().text();
+        return nullopt;
+    }
+    return getUserById(query.lastInsertId().toLongLong());
+}
+
+// ─── File MetaData ────────────────────────────────────────────────────────────
--- database/databasemanager.h
+++ database/databasemanager.h
@@ -3,11 +3,36 @@
 
 #include <QObject>
 #include <QSqlQuery>
+#include <optional>
+
+#include "utils/user.h"
+
+using std::optional;
+using std::nullopt;
 
 class DatabaseManager : public QObject
 {
 public:
     DatabaseManager(QObject *parent);
+    bool open(const QString &dbName);
+    bool isOpen() const;
+    void close();
+
+    //---------Users---------
+    optional<User> getUserByUsername(const QString &username);
+    optional<User> createUser(const QString &username, const QString &email, const QString &hash, const QString &salt, qint64 defaultQuota);
+    optional<User> getUserById(const qint64 &id);
+    qint64 calcUsedBytes(const qint64 &id);
+
+    //---------Sessions---------
+    bool createSession(qint64 &userId, const QString &jwtToken, qint64 expiresAt);
+    bool deleteSession(const QString &jwtToken);
+    bool isSessionValid(const QString &jwtToken);
+
+private:
+    bool createTables();
+    QSqlDatabase m_db;
+    QString m_connectionName;
 };
 
 #endif // DATABASEMANAGER_H
